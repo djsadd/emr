abf5aae936c1f5d4ee032ab16e911a2d486f5bf3
Erasyl <erasil.bakhytgan@gmail.com>
2024-06-07 10:54:27 +0500

    first commit

---
+++ sites/default/documents/smarty/main/28f9c9d8486a1965a96b915e7793bfcc1d0d34d3_0.file.general_fragment.html.php
@@ -0,0 +1,77 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-06-04 08:00:46
+  from 'C:\OSPanel\domains\openemr.kz\templates\prescription\general_fragment.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_665e9f7ecc1b51_08504050',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '28f9c9d8486a1965a96b915e7793bfcc1d0d34d3' => 
+    array (
+      0 => 'C:\\OSPanel\\domains\\openemr.kz\\templates\\prescription\\general_fragment.html',
+      1 => 1716883575,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_665e9f7ecc1b51_08504050 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'C:\\OSPanel\\domains\\openemr.kz\\library\\smarty\\plugins\\function.xlt.php','function'=>'smarty_function_xlt',),));
+if (empty($_smarty_tpl->tpl_vars['prescriptions']->value)) {
+echo smarty_function_xlt(array('t'=>'None'),$_smarty_tpl);?>
+
+<?php } else { ?>
+<div class="table-responsive">
+    <table class="table table-sm table-striped">
+        <thead>
+            <tr>
+                <th><?php echo smarty_function_xlt(array('t'=>'Drug'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Details'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Qty'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Refills'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Filled'),$_smarty_tpl);?>
+</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['prescriptions']->value, 'prescription');
+$_smarty_tpl->tpl_vars['prescription']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['prescription']->value) {
+$_smarty_tpl->tpl_vars['prescription']->do_else = false;
+?>
+            <?php if ($_smarty_tpl->tpl_vars['prescription']->value->get_active() > 0) {?>
+            <tr>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->drug);?>
+&nbsp;</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_size());
+echo text($_smarty_tpl->tpl_vars['prescription']->value->get_unit_display());?>
+&nbsp;
+                    <?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_dosage_display());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_quantity());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_refills());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_date_added());?>
+</td>
+            </tr>
+            <?php }?>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+        </tbody>
+    </table>
+</div>
+<?php }
+}
+}
